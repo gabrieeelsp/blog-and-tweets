94c8a1f1e67c5e066fb7827639bb7e3ea73808f3
gabrieeelsp <gabrieeelsp@gmail.com>
2020-06-14 20:33:03 -0300

    Entry model (Create, Edit, Show), User Profie page, Initial data with Seeders

---
+++ app/Http/Controllers/GuessController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Entry;
+class GuessController extends Controller
+{
+    public function index()
+    {
+        $entries = Entry::with('user')
+            ->orderByDesc('created_at')
+            ->orderByDesc('id')
+            ->paginate(10);
+        return view('welcome', compact('entries'));
+    }
+
+    public function show(Entry $entry)
+    {
+        return view('entries.show', compact('entry'));
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'gabriel',
+            'email' => 'contacto@plastitodo.com.ar',
+            'password' => bcrypt('roscen'),
+        ]);
+
+        factory(User::class, 10)->create();
+    }
+}
